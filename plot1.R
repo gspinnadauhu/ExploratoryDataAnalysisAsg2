79232604b8171fcdb49bdbae4310f0deb6728f09
gspinnadauhu <markusalang@gmail.com>
2017-02-10 17:29:15 -0600

    finishing plot

--- plot1.R
+++ plot1.R
@@ -1,3 +1,9 @@
 #Loading the data
 NEI <- readRDS("./data/summarySCC_PM25.rds")
 SCC <- readRDS("./data/Source_Classification_Code.rds")
+#summing Emissions by year
+NEIplot1<-with(NEI,tapply(Emissions,as.factor(year),sum))
+barplot(NEIplot1,
+        main="Plot1: Total PM2.5 emissions",
+        xlab="Year",
+        ylab="PM 2.5 in tons")
