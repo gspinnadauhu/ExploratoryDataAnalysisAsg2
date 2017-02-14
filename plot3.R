58d65e6914d08b3346a4ef5a8b58538300d4c230
gspinnadauhu <markusalang@gmail.com>
2017-02-13 21:36:52 -0600

    finishing plot 3

--- plot3.R
+++ plot3.R
@@ -8,10 +8,10 @@ bmorecity_type<-NEI %>%
   filter(fips=="24510") %>%
   select(fips,Emissions,type,year)
 ##sum by type by year
-bmorecity_type<-with(bmorecity_type,tapply(Emissions,list(as.factor(year),as.factor(type)),sum))
-bmorecity_type<-as.data.frame(bmorecity_type)
+bmorecity_type<-with(bmorecity_type,aggregate(Emissions,by=list(year,type),sum))
+names(bmorecity_type)<-c("Year","Type","Emissions")
 #plot
 library(ggplot2)
-ggplot(data=bmorecity_type,aes(x=year,y=Emissions))+
-  geom_bar()+
-  facet_grid(.~type)
+ggplot(data=bmorecity_type,aes(x=as.factor(Year),y=Emissions))+
+  geom_bar(stat="identity")+
+  facet_grid(.~Type)
