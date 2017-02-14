ff1544c14173b6c1eadaff7ac8df58fc52e7a4f5
gspinnadauhu <markusalang@gmail.com>
2017-02-13 22:55:38 -0600

    finishing plot5

--- plot5.R
+++ plot5.R
@@ -1,3 +1,20 @@
 #Loading the data
 NEI <- readRDS("./data/summarySCC_PM25.rds")
 SCC <- readRDS("./data/Source_Classification_Code.rds")
+#subsetting for vehicle related sources and Baltimore City
+##subsetting EI.Sector containg "Vehicle"
+library(dplyr)
+vehicle_SCC<-SCC %>%
+  filter(grepl('Vehicle',EI.Sector)) %>%
+  select(SCC)
+##subset NEI for Bmore City and based on vehicle_SCC
+NEI_vehicle<-NEI %>%
+  filter(SCC %in% vehicle_SCC$SCC & fips=="24510") %>%
+  select(Emissions,year)
+NEI_vehicle$year<-as.factor(NEI_vehicle$year)
+#plot
+ggplot(data=NEI_vehicle,aes(x=year,y=Emissions))+
+  stat_summary(fun.y=sum,geom="bar")+
+  labs(y="PM 2.5 in tons",
+       x="Year",
+       title="Plot5: U.S. Emissions from Vehicles 1999-2008")
